f527b9683bd891aba933db131905aeaf6c884006
Leigh <1142961+leighgordy@users.noreply.github.com>
2026-03-23 08:27:51 +0000

    Removed try catch from copy-file.ts, if it breaks I want the app to completely fail and I want the error visible

--- code/blog-generation/copy-file.ts
+++ code/blog-generation/copy-file.ts
@@ -1,11 +1,7 @@
 import * as fs from "fs";
 
 const copyFile = (source: string, destination: string): void => {
-  try {
     fs.copyFileSync(source, destination);
-  } catch (error) {
-    console.error(`Error copying file: ${error}`);
-  }
 };
 
 export default copyFile;
